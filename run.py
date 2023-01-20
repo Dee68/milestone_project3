a8a792c733ee61d2769f544c96d04d4b360529e9
Dee68 <ddimie283@gmail.com>
2023-01-20 17:23:56 +0000

    Add function to enable animation of letters(word_wrap) and function to display options.

--- run.py
+++ run.py
@@ -20,3 +20,31 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('atm_abc')
 
+
+def word_wrap(words):
+    """ Delays the output of characters on terminal by some milliseconds"""
+    for c in words:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+def show_menu():
+    """ Displays the menu to a user """
+    word_wrap(f"""{Fore.YELLOW}
+        |====================================================|
+        |                                                    |
+        |     WELCOME TO ABC ATM BANKING SYSTEM              |
+        |                                                    |
+        |   Please choose one of the following options:      |
+        |                                                    |
+        |     1. Create an account                           |
+        |     2. Enter a deposit to account                  |
+        |     3. Withdraw from account                       |
+        |     4. Show account details                        |
+        |     5. Show Transactions                           | 
+        |     6. Exit                                        |    
+        |====================================================|""")
+    print("\n\n")
+
+    
